60773b81f254b3ff8f7fe4f2fa265fd93d098b8e
Michal Maminski <michal.maminski@gmail.com>
2017-11-09 18:10:59 +0100

    Add root component as host for timers and timers form fields

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,7 +2,17 @@ class Demo extends React.Component {
     render() {
         console.log("React is rendering component!!!");
         return (<h1>This is view from React component!</h1>);
+class TimersDashboard extends React.Component {
+    render() {
+        return (
+            <div className="ui three column centered grid">
+                <div>
+                    <EditableTimerList />
+                    <ToggleableTimerForm isOpen={true} />
+                </div>
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<Demo/>, document.getElementById('content'));
+ReactDOM.render(<TimersDashboard />, document.getElementById('content'));
