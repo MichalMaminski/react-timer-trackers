9897176d99c8c81ecd731fdda4fa3fdd3923bf90
Michal Maminski <michal.maminski@gmail.com>
2017-11-11 20:14:53 +0100

    Add body-parser and jsonFile dependencies - libs which allows read/write to server files and parse json requests

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const jsonfile = require('jsonfile');
+const bodyParser = require('body-parser');
 const expressHost = express()
 
 expressHost.set('port', process.env.PORT || 8888);
 
 expressHost.use('/', express.static(path.join(__dirname, 'src/public')));
+expressHost.use(bodyParser.json());
+expressHost.use(bodyParser.urlencoded({ extended: true }));
 expressHost.use('/node/scripts/', express.static(path.join(__dirname, 'node_modules')));
 expressHost.use('/node/styles/', express.static(path.join(__dirname, 'node_modules')));
 
