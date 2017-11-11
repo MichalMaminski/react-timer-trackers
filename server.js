3bb67983e7da2ef18930ab04c14449a830a4830b
Michal Maminski <michal.maminski@gmail.com>
2017-11-11 20:58:59 +0100

    Add new web api server methods, for returning stored timers and posting new one

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ expressHost.use(bodyParser.urlencoded({ extended: true }));
 expressHost.use('/node/scripts/', express.static(path.join(__dirname, 'node_modules')));
 expressHost.use('/node/styles/', express.static(path.join(__dirname, 'node_modules')));
 
+expressHost.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+
+expressHost.get('/api/timers', (req, res) => {
+    jsonfile.readFile(pathToJsonFileWithTimers, (err, timers) => {
+        res.setHeader('Cache-Control', 'no-cache');
+        res.json(timers);
+    });
+});
+
+expressHost.post('/api/timers', (req, res) => {
+    jsonfile.readFile(pathToJsonFileWithTimers, (err, timers) => {
+        timers.push(req.body);
+        jsonfile.writeFile(pathToJsonFileWithTimers, timers, { spaces: 4 }, () => {
+            res.setHeader('Cache-Control', 'no-cache');
+            res.json(timers);
+        });
+    });
+});
+
 expressHost.listen(expressHost.get('port'), () => {
     console.log(`Find the server at: http://localhost:${expressHost.get('port')}/`);
 });
